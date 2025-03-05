c7f7b2e0d065eed47239bb892da536ca77297578
seyitbugraerden <seyitbugraerden@gmail.com>
2025-03-05 23:55:38 +0300

    faaliyet alanlarımız fix

--- app/(pages)/faaliyet-alanlarimiz/[slug]/page.tsx
+++ app/(pages)/faaliyet-alanlarimiz/[slug]/page.tsx
@@ -12,6 +12,26 @@ export default async function Page({
   return (
     <>
       <Banner text={lastData} />
+      <section className="max-contain flex gap-24">
+        <div className="">
+          <div className="sticky top-12">
+            <h3 className="text-3xl font-semibold">Faaliyet Alanları</h3>
+            <ul className="mt-3">
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex-1 h-[200dvh]">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, eos.
+        </div>
+      </section>
     </>
   );
 }
