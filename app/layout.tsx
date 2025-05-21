7c22ed43adbc5be0fc2d879c128b61d25656e479
seyitbugraerden <seyitbugraerden@gmail.com>
2025-05-21 14:09:46 +0300

    favicon updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+      </head>
       <body className={`${open.className} antialiased overflow-x-hidden`}>
         <Header />
         {children}
